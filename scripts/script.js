d77c1520e0cf8dcc5c011d1cec3e64037709b94b
Jerry <jerry.tamlander@helsinki.fi>
2017-05-27 12:14:47 +0300

    Adds to html and CSS

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,45 @@
 $(document).ready(function(){
     
-    var lat;
-    var lon;
+    var latitude;
+    var longitude;
     
       if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            lat = position.coords.latitude;
-            lon = position.coords.longitude;
-            $("#data").html("latitude: " + lat + " longitude: " + lon);
-        console.log("la" + lat);
+        navigator.geolocation.getCurrentPosition(function(geoposition) {
+            latitude = geoposition.coords.latitude;
+            longitude = geoposition.coords.longitude;
+            $("#data").html("latitude: " + latitude + " longitude: " + longitude);
         
-  });
+            
+        var API_KEY = "9d1a5d7fd040aed65637d4dd56c39eeb";
+        var api = "http://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&appid="+API_KEY;
+        console.log(api);    
+            
+            $.getJSON(api, function(data) {
+                var celciusSelected=true;
+                var celciusTemperature = (data.main.temp - 273.15).toFixed(1);
+                var fahrenheitTemperature = (data.main.temp * 9/5 - 459.67).toFixed(1);	
+                
+                
+                $("#location").html(data.name); $("#weathertype").html(data.weather[0].description);
+                
+                $("#temperature").html(celciusTemperature + " &#8451");                
+
+                $("#temperature").click(function() {
+                    if(celciusSelected===false) { 
+                       $("#temperature").html(celciusTemperature + " &#8451");
+                    celciusSelected=true;
+                    } else{
+                    $("#temperature").html(fahrenheitTemperature + " &#8457");
+                    celciusSelectede=false;
+                }
+                });
+                
+                
+                $("#humidity").html(data.main.humidity + " %");
+                $("#airpressure").html(data.main.pressure + " hPa");
+                $("#windspeed").html(data.wind.speed + " m/s");
+                
+            });
+            });
       }
-          
-    var api = "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid=9d1a5d7fd040aed65637d4dd56c39eeb";
-    console.log("la2" + lat);
-    console.log(api);
-    
 });
